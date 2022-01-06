d80fcbe558d6364c5d66e6aff4d6f4d660c436c9
kronosGR <geo.elgeo@gmail.com>
2022-01-06 11:30:22 +0100

    added dynamic page for event

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,21 +1,20 @@
-import { useRouter } from 'next/router';
-import { getEventById } from '../../dummy-data';
 import { Fragment } from 'react';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
+import {
+  getEventById,
+  getFeaturedEvents
+} from '../../helpers/api-utils';
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const event = props.selectedEvent[0];
 
   if (!event) {
-    return <ErrorAlert>
-      <p>No event found!</p>;
-    </ErrorAlert>;
+    return <div className="centered">
+      <p>Loading...</p>;
+    </div>;
   }
 
   return (
@@ -28,6 +27,28 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event
+    },
+    revalidate: 30
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getFeaturedEvents();
+
+  const paths = events.map(event => ({ params: { eventId: event.id } }));
+  return {
+    paths: paths,
+    fallback: 'blocking' // blocks until page generated
+  };
 }
 
 export default EventDetailPage;
