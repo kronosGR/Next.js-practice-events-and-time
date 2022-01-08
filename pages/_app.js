feb9224ebfe79a7bbeed4b308d62c0ac172405b3
kronosGR <geo.elgeo@gmail.com>
2022-01-08 13:09:59 +0100

    Added HEAD _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import Layout from '../components/layout/layout';
 
@@ -6,6 +7,9 @@ function MyApp({
                  pageProps
                }) {
   return <Layout>
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+    </Head>
     <Component {...pageProps} />
   </Layout>;
 }
