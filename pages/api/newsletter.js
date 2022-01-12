e9109625defbb3c56a38bbbc82f6cc9702f3d80b
kronosGR <geo.elgeo@gmail.com>
2022-01-12 06:27:00 +0100

    save newsletter to mongodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -7,8 +9,19 @@ function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
-    res.status(201).json({message:'Signed up!'})
+    const client = await MongoClient.connect(
+      'mongodb+srv://nextevent:QlLJ7rsrPBNf1wRS@cluster0.hrnez.mongodb.net/newsletter?retryWrites=true&w=majority'
+    );
+
+    const db = client.db();
+
+    await db.collection('emails').insertOne({
+      email: userEmail,
+    });
+
+    client.close();
+
+    res.status(201).json({ message: 'Signed up!' });
   }
 }
 
