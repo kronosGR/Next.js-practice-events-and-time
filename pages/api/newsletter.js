511e5289b117e372669d58751bc7721eb86feae6
kronosGR <geo.elgeo@gmail.com>
2022-01-13 07:09:58 +0100

    Error handling to newsletter route

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,19 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://nextevent:QlLJ7rsrPBNf1wRS@cluster0.hrnez.mongodb.net/events?retryWrites=true&w=majority'
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+
+  await db.collection('newsletter').insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -9,17 +23,22 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://nextevent:QlLJ7rsrPBNf1wRS@cluster0.hrnez.mongodb.net/events?retryWrites=true&w=majority'
-    );
+    let client;
 
-    const db = client.db();
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to DB failed' });
+      return;
+    }
 
-    await db.collection('newsletter').insertOne({
-      email: userEmail,
-    });
-
-    client.close();
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting to DB failed' });
+      return;
+    }
 
     res.status(201).json({ message: 'Signed up!' });
   }
